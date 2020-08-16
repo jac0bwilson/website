587b6fb58c1274c9ceaad0ada61ec41575d788ce
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-08-16 23:40:37 +0100

    Save theme preference locally

--- darkModeToggle.js
+++ darkModeToggle.js
@@ -1,12 +1,21 @@
 window.onload = function() {
     const btn = document.getElementById("dark-mode-toggle");
     const icon = document.getElementById("dark-mode-icon");
+    const darkClass = "dark-mode";
+    
+    function toggleTheme() {
+        document.body.classList.toggle(darkClass);
+        icon.classList.toggle("fa-moon")
+        icon.classList.toggle("fa-sun")
+
+        localStorage.setItem(darkClass, document.body.classList.contains(darkClass));
+    }
+
+    if (localStorage.getItem(darkClass) == "true") {
+        toggleTheme();
+    }
 
     if (btn) {
-        btn.addEventListener("click", function () {
-            document.body.classList.toggle("dark-mode");
-            icon.classList.toggle("fa-moon")
-            icon.classList.toggle("fa-sun")
-        });
+        btn.addEventListener("click", toggleTheme);
     }
 }
